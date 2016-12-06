32257922039fc5016cc0de795a658947f4ce7370
Celena T <chibiynm@gmail.com>
2016-12-05 21:35:39 -0500

    Add people page

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+var allPeople = document.getElementsByClassName('person');
+
 function hide(navigateTo) {
-  var pages = ['home', 'events', 'people', 'results', 'login', 'signup'];
+  var pages = ['home', 'dashboard', 'events', 'people', 'results', 'login', 'signup'];
 
   var index = pages.indexOf(navigateTo);
   pages.splice(index, 1);
@@ -9,4 +11,30 @@ function hide(navigateTo) {
   });
 
   $('#' + navigateTo).show();
+}
+
+function search(query) {
+  console.log(query);
+  if (query === '') {
+    $('.person').show();
+    $('#show-all-btn').hide();
+    return;
+  }
+
+  var text = [];
+  $('.person').hide();
+
+  for (var i = 0; i < allPeople.length; i++) {
+    text.push(allPeople[i].innerText.toLowerCase());
+  }
+
+  console.log(text);
+
+  for (var j = 0; j < text.length; j++) {
+    if (text[j].includes(query.toLowerCase())) {
+      console.log(text[j]);
+      $('.person').eq(j).show();
+      $('#show-all-btn').show();
+    }
+  }
 }
